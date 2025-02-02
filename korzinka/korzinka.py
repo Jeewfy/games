ee0099719c584c9da5d2befc77d5530739166f48
Jeewfy <166714146+Jeewfy@users.noreply.github.com>
2025-02-02 12:56:18 +0000

    yo

---
+++ korzinka/korzinka.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+
+pygame.int()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Cath the apple game")
+
+basket_img = pygame.image.load('basket.png')
+apple_img = pygame.image.load('apple.png')
+
+basket_width, basket_height = 50, 50
+apple_width, apple_height - basket_heigth - 10
+
+
+
+
+baket_x = screen_width // 2 - basket_width // 2
